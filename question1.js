7be73a936313fba9d16e9f08f564cc6c67970a73
haya <hydrangeaaaa1@users.noreply.github.com>
2021-01-05 20:39:07 +0300

    done with bonus

--- question1.js
+++ question1.js
@@ -1,1 +1,17 @@
 // قم بحل السؤال الأول هنا
+let pets = ["cat" , "bird" , "rabbit" ,"hamster","dog"];
+console.log(pets);
+pets.pop();
+pets.push("fish");
+pets[2] = "parrot";
+console.log(pets);
+console.log(pets.length);
+
+let cats = {
+    name:"katy",
+    age:"2",
+    kind:"Ragdoll",
+    emoji:"😸"
+}
+console.log(cats);
+console.log(cats.age);
